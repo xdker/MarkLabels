137550f6f983c4f20e07a62bdba0660571801f0f
xdker <z779867650@gmail.com>
2021-02-01 14:34:26 +0800

    路径对不上的问题

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ import os
 import cgitb
 
 import sys
-
+sys.argv.append("-disable-web-security")
 os.environ['QT_MAC_WANTS_LAYER'] = '1'  # 解决MAC big sur 无法打开的问题pyqt5.15.2无需设置
 cgitb.enable(display=1, logdir=Common.CACHE_DIR)
 
 if __name__ == '__main__':
-    app = QApplication([])
+    app = QApplication(sys.argv)
     main_win = MainWindow()
     app.exec_()
